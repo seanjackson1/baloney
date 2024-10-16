d6c63cb112530b1a1eb9a9261612d998ae4edb6e
seanjackson1 <seanjackson1230@gmail.com>
2024-10-15 22:41:26 -0400

    Update Baloney.java

--- Baloney.java
+++ Baloney.java
@@ -5,31 +5,31 @@ import java.util.*;
 public class Baloney {
   public static void main(String[] args) {
 
-    // boolean showAllCards = true;
-    // int delayInMilliseconds = 200;
-    // Display display = new Display(showAllCards, delayInMilliseconds);
-    // Strategy[] strategies = { new SeanJackson(), new Human(display), new
-    // SeanJackson(), new SeanJackson()
-    // };
-    // int winner = Game.play(display, strategies);
-    // System.out.println("winner = " + winner);
+    boolean showAllCards = true;
+    int delayInMilliseconds = 200;
+    Display display = new Display(showAllCards, delayInMilliseconds);
+    Strategy[] strategies = { new SeanJackson(), new Human(display), new
+    SeanJackson(), new SeanJackson()
+    };
+    int winner = Game.play(display, strategies);
+    System.out.println("winner = " + winner);
 
-    // for (int i = 0; i < 10; i++) {
-    // System.out.println("Game: " + i + " Winner = " + Game.play(null,
-    // strategies));
-    // }
-
-    Strategy[] strategies = {
-        new SeanJackson(), new SeanJackson(), new SeanJackson(), new SeanJackson() };
-
-    int[] seats = new int[4];
-    for (int i = 1; i < 100; i++) {
-      int winner = Game.play(null, strategies);
-      seats[winner]++;
-      System.out.println("Game: " + i + " Winner = " + winner);
+    for (int i = 0; i < 10; i++) {
+    System.out.println("Game: " + i + " Winner = " + Game.play(null,
+    strategies));
     }
-    for (int i = 0; i < 4; i++)
-      System.out.println("Seat: " + i + " Games Won = " + seats[i]);
+
+    // Strategy[] strategies = {
+    //     new SeanJackson(), new SeanJackson(), new SeanJackson(), new SeanJackson() };
+
+    // int[] seats = new int[4];
+    // for (int i = 1; i < 100; i++) {
+    //   int winner = Game.play(null, strategies);
+    //   seats[winner]++;
+    //   System.out.println("Game: " + i + " Winner = " + winner);
+    // }
+    // for (int i = 0; i < 4; i++)
+    //   System.out.println("Seat: " + i + " Games Won = " + seats[i]);
 
     // new RemoteTable("10.13.99.50", new SeanJackson());
     // new RemoteTable("10.13.99.50", new SeanJacksonTheBestIfNoOther());
